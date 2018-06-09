b73bd31706f64dadb23b8d888dc6aa47fb680c14
ruefa <ruef.alex@gmail.com>
2018-06-09 16:43:13 -0700

    SVC model fitted

---
+++ svcModel.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+import dateutil.parser
+import math
+import util
+from sklearn import svm
+import graphviz
+
+convert = lambda x: int(dateutil.parser.parse(x).hour)
+
+def create_group():
+    subject_files = ['Subject_1.csv', 'Subject_4.csv', 'Subject_6.csv', 'Subject_9.csv' ]
+
+    samples = np.empty(shape=(0,64))
+
+    for f in subject_files:
+        sample = np.loadtxt('./data/'+f, converters={0:convert}, delimiter=',')
+        s = util.reshape_sample(sample)
+        samples = np.vstack((samples, s))
+
+    return samples
+
+examples = create_group()
+clf = svm.SVC()
+clf.fit(examples[:,:-1], examples[:,-1])
