c16b6e816317b7c2fec6e2bc44d8d3706d7d238d
shogerr <shogerr@oregonstate.edu>
2018-06-09 14:42:53 -0700

    fix counting of reshape

--- util.py
+++ util.py
@@ -5,15 +5,17 @@ def reshape_sample(s):
     # reverse samples and break up sample set where last column is true
     t = np.split(np.flip(s[:,0:9],0), np.where(np.flip(s,0)[:,9] == 1)[0][0:])
 
-    # remove chunks with fewer than 70 elements
+    # remove chunks with fewer than 63 elements
     t = [x for x in t if x.size > 62]
 
     # create set for all samples
     sample_set = np.empty(shape=(0,64))
     for i in range(len(t)):
         sample_local = np.empty(shape=(0,63))
+        # Get the number of rows that are useful as examples
         num_rows = math.floor(t[i].shape[0]/7)
-        for j in range(num_rows):
+
+        for j in range(0, num_rows*7, 7):
             u = np.flip(t[i][j:j+7,:], 0).reshape(1, 63)
             sample_local = np.vstack((sample_local, u))
 
