691c087a8a37c1f43779fa8be8a06e4a8a6da8bc
ruefa <ruef.alex@gmail.com>
2018-06-09 19:29:29 -0700

    added testing predictions

--- treeModel.py
+++ treeModel.py
@@ -19,10 +19,50 @@ def create_group():
 
     return samples
 
+def create_test_group():
+    f = 'general_test_instances.csv'
+    sample = np.loadtxt('./data/'+f, delimiter=',')
+    s = util.reshape_test(sample)
+
+    return s
+
+
+def testRateRun(examples):
+    dataSplit = 1500
+    clf = tree.DecisionTreeClassifier(class_weight='balanced')
+    clf = clf.fit(examples[:dataSplit,:-1], examples[:dataSplit,-1])
+    print('test data predictions:')
+    successRate(clf.predict(examples[dataSplit:,:-1]), examples[dataSplit:,-1])
+
+
+def successRate(expected, actual):
+    success = 0
+    for i in range(len(actual)):
+        if expected[i] == actual[i]:
+            success += 1
+    print('correct: ' + str(success), 'total: ' + str(len(expected)), 'rate: ' + str(success/len(expected)))
+
+
+def createTreeImage(clf):
+    dotData = tree.export_graphviz(clf, out_file=None)
+    graph = graphviz.Source(dotData)
+    graph.render('tree')
+
+def createPredictionsCSV(clf, testData):
+    predictions = clf.predict(testData)
+    predProb = clf.predict_proba(testData)
+    with open('tree_predictions.csv', 'w+') as f:
+        for i in range(len(predictions)):
+            prob = max(predProb[i])
+            f.write(str(predictions[i]) + ',' + str(predProb[i]) + '\n')
+
+
+
 examples = create_group()
 clf = tree.DecisionTreeClassifier(class_weight='balanced')
 clf = clf.fit(examples[:,:-1], examples[:,-1])
+successRate(clf.predict(examples[:,:-1]), examples[:,-1])
+testExamples = create_test_group()
+createPredictionsCSV(clf, testExamples)
 
-dotData = tree.export_graphviz(clf, out_file=None)
-graph = graphviz.Source(dotData)
-graph.render('tree')
+testRateRun(examples)
