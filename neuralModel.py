3676f1ab26717af365b65b8f6bf81dfbb6190cc6
ruefa <ruef.alex@gmail.com>
2018-06-12 14:46:11 -0700

    testing neural net

---
+++ neuralModel.py
@@ -0,0 +1,60 @@
+import numpy as np
+import dateutil.parser
+import math
+import util
+from sklearn import neural_network
+import graphviz
+
+convert = lambda x: int(dateutil.parser.parse(x).hour)
+
+def create_group():
+    subject_files = ['Subject_1.csv', 'Subject_4.csv', 'Subject_6.csv', 'Subject_9.csv' ]
+
+    samples = np.empty(shape=(0,64))
+
+    for f in subject_files:
+        sample = np.loadtxt('./data/'+f, converters={0:convert}, delimiter=',')
+        s = util.reshape_sample(sample)
+        samples = np.vstack((samples, s))
+
+    return samples
+
+def create_test_group():
+    f = 'general_test_instances.csv'
+    sample = np.loadtxt('./data/'+f, delimiter=',')
+    s = util.reshape_test(sample)
+
+    return s
+
+
+def testRateRun(examples):
+    dataSplit = 1500
+    clf = neural_network.MLPClassifier()
+    clf = clf.fit(examples[:dataSplit,:-1], examples[:dataSplit,-1])
+    print('test data predictions:')
+    util.successRate(clf.predict(examples[dataSplit:,:-1]), examples[dataSplit:,-1])
+
+
+def createTreeImage(clf):
+    dotData = tree.export_graphviz(clf, out_file=None)
+    graph = graphviz.Source(dotData)
+    graph.render('tree')
+
+
+def createPredictionsCSV(clf, testData, fileName):
+    predictions = clf.predict(testData)
+    predProb = clf.predict_proba(testData)
+    with open(fileName, 'w+') as f:
+        for i in range(len(predictions)):
+            prob = max(predProb[i])
+            f.write(str(predictions[i]) + ',' + str(predProb[i]) + '\n')
+
+
+examples = create_group()
+clf = neural_network.MLPClassifier()
+clf = clf.fit(examples[:,:-1], examples[:,-1])
+util.successRate(clf.predict(examples[:,:-1]), examples[:,-1])
+testExamples = create_test_group()
+createPredictionsCSV(clf, testExamples, 'neural_net_predictions.csv')
+
+testRateRun(examples)
